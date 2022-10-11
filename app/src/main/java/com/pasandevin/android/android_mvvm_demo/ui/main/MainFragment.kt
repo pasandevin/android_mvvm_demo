ae7e6fddbd8832c72fc1fb9060648afa82d401cf
pasandevin <jayawardene.pasandevin@gmail.com>
2022-10-11 22:33:59 +0530

    implemented simple logic to update ui based on inputs

--- app/src/main/java/com/pasandevin/android/android_mvvm_demo/ui/main/MainFragment.kt
+++ app/src/main/java/com/pasandevin/android/android_mvvm_demo/ui/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.pasandevin.android.android_mvvm_demo.ui.main
 
+import android.content.ContentValues.TAG
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pasandevin.android.android_mvvm_demo.R
+import com.pasandevin.android.android_mvvm_demo.databinding.FragmentMainBinding
+import kotlin.math.log
 
 class MainFragment : Fragment() {
 
@@ -15,18 +19,33 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+
+//        binding.vm = viewModel
+
+        viewModel.message.observe(viewLifecycleOwner) {
+            binding.reflectedMessage.text = it
+        }
+
+        binding.submitButton.setOnClickListener {
+            viewModel.updateMessage(binding.orginalMessage.text.toString())
+            binding.reflectedMessage.text = viewModel.message.value
+        }
+
+        return binding.root
     }
 
 }
