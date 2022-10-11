e0d54dd8904ee00a4274ac8770b11e41c6590bda
pasandevin <jayawardene.pasandevin@gmail.com>
2022-10-11 19:23:22 +0530

    Initial commit

---
+++ app/src/main/java/com/pasandevin/android/android_mvvm_demo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.pasandevin.android.android_mvvm_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pasandevin.android.android_mvvm_demo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/pasandevin/android/android_mvvm_demo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.pasandevin.android.android_mvvm_demo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
